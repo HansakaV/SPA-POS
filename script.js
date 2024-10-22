8777a9bb964e13b3db7314b887a1758520c29402
HansakaV <maheshhansaka628@gmail.com>
2024-10-22 21:50:25 +0530

    updated script

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
-let customer_array =[]
+// customer array
+let customer_array = [];
+
 
 const loadCustomerTable = () => {
-    $('#customerTableBody').empty()
+    $("#customerTableBody").empty();
     customer_array.map((item, index) => {
         console.log(item);
-        let data = `<tr>
-        <td>${item.first_name}</td>
-        <td>${item.last_name}</td>
-        <td>${item.mobile}</td>
-        <td>${item.email}</td>
-        <td>${item.address}</td>
-    </tr>`
-        $('#customerTableBody').append(data)
+        let data = `<tr><td>${item.first_name}</td><td>${item.last_name}</td><td>${item.mobile}</td><td>${item.email}</td><td>${item.address}</td></tr>`
+        $("#customerTableBody").append(data);
+    })
+}
 
-    });
-};
+// Add Customer Button
+$("#customer_add_btn").on("click", function() {
+    let first_name = $('#firstName').val();
+    let last_name = $('#lastName').val();
+    let mobile = $('#mobile').val();
+    let email = $('#email').val();
+    let address = $('#address').val();
 
+    console.log("first_name: ", first_name);
+    console.log("last_name: ", last_name);
+    console.log("mobile: ", mobile);
+    console.log("email: ", email);
+    console.log("address: ", address);
 
-$("#add-customer").on("click " ,function (){
-   let first_name = $('#firstName').val();
-   let last_name = $('#lastName').val();
-   let mobile = $('#mobile').val();
-   let email = $('#email').val();
-   let address = $('#address').val();
-   console.log(first_name,last_name,mobile,email,address)
+    let customer = {
+        id: customer_array.length + 1,
+        first_name: first_name,
+        last_name: last_name,
+        mobile: mobile,
+        email: email,
+        address: address
+    };
 
-
-
-    let id = customer_array.length+1;
-     let customer = {
-         id: id,
-         first_name: first_name,
-         last_name: last_name,
-         mobile: mobile,
-         email: email,
-         address: address
-    }
     customer_array.push(customer);
-    loadCustomerTable()
 
+    loadCustomerTable();
 
-})
+    // // create table row
+    // let data = `<tr><td>${first_name}</td><td>${last_name}</td><td>${mobile}</td><td>${email}</td><td>${address}</td></tr>`
+    // $("#customerTableBody").append(data);
+});
 
